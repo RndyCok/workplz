aba0cc6d90b9734ca7093545e4f334e740e7ce54
RndyCok <randygans071@gmail.com>
2022-07-20 02:39:11 +0700

    kw

---
+++ plugins/sourcecode.js
@@ -0,0 +1,22 @@
+let handler = async (m, { conn }) => {
+	
+	// Link Script jgn di ganti ya pliss
+    // Kalo mau masukin github kamu tinggal add aja link githubnya tapi jangan di hapus sumber script!
+    // Jangan Ngeyell..
+    // Kalo ngeyel gk bakal gw up sc lgi.
+    
+    let txt = `
+Bot ini menggunakan script github
+
+Mau scriptnya Cek di youtube Nih https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ
+Script original By Family-MD
+`
+     conn.reply(m.chat, txt, m)
+}
+handler.help = ['sourcecode']
+handler.tags = ['info']
+handler.command = /^(sc(ript(bot)?)?|sourcecode)$/i
+
+module.exports = handler
+
+
